2a232ee33dd4bbcc79597d50254fe7d3f65adfbb
Kaif <kaif@example.com>
2026-03-30 00:20:31 +0530

    commit updated thrice.

--- src/main/java/Demo/DemoTest.java
+++ src/main/java/Demo/DemoTest.java
@@ -2,11 +2,15 @@ package Demo;
 
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world updated thrice");
         featureB();
+        featureC();
     }
 
     public static void featureB(){
         System.out.println("payment feature");
+    }
+    public static  void featureC(){
+        System.out.println("Register feature");
     }
 }
